9f9b37fc74074fdb9c415f26048923293b691b23
vohungvi <vohungvi@vohungvi.com>
2018-12-09 11:54:03 +0700

    add code

---
+++ IPSSCs/Util.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+namespace IPSS
+{
+    class Util
+    {
+        public static RegistryKey g_regKey = Registry.CurrentUser.OpenSubKey("Software\\IPSS", true);
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        public static void CheckRegistry()
+        {
+            g_regKey = Registry.CurrentUser.OpenSubKey("Software\\IPSS", true);
+            if(g_regKey == null)
+            {
+                Registry.CurrentUser.OpenSubKey("SOFTWARE", true).CreateSubKey("IPSS");
+                g_regKey = Registry.CurrentUser.OpenSubKey("Software\\IPSS", true);
+            }
+        }        
+        /////////////////////////////////////////////////////////////////////////////////////////////////        
+        public static void WriteReg(string key, object value)
+        {
+            CheckRegistry();
+            g_regKey.SetValue(key, value);
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////        
+        public static object ReadReg(string key)
+        {
+            if (g_regKey.GetValue(key) == null)
+                return "";
+            else
+                return g_regKey.GetValue(key);
+        }
+    }
+}
